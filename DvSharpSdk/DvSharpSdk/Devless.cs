122270c332500cc051eaff19da455a1a61b4e86e
kayaddo3 <kayaddo3@gmail.com>
2018-02-24 06:14:32 -0800

    Add files via upload

---
+++ DvSharpSdk/DvSharpSdk/Devless.cs
@@ -0,0 +1,136 @@
+﻿using DvSharpSdk.Util;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+
+namespace DvSharpSdk
+{
+    public class Devless
+    {
+        private String rootUrl, tokenValue, devlessUserToken = "", where = "", orderBy = "id", empty;
+        private int size = -1;
+        private static String TAG = "DevLess";
+
+        public Devless(String rootUrl, String tokenValue)
+        {
+            //this.mContext = mContext;
+            this.rootUrl = rootUrl; //rootUrl => https://shrouded-shelf-94964.herokuapp.com
+            this.tokenValue = tokenValue; // tokenValue=> 961ae615906605e7b4a45846e85b9db6
+
+        }
+
+        //public String getData(String serviceName, String tableName, IGetDataResponse getDataResponse)
+        public void getData(String serviceName, String tableName)
+        {
+            string onioin = "/db?table="; string tablename = tableName;
+            string GetRequestUrl = serviceName + onioin + tableName;
+
+            //using RESTSHARP
+
+            var client = new RestClient(new DevlessBuilder().getUrl(rootUrl, serviceName, tableName));
+            var request = new RestRequest(Method.GET);
+            //request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("devless-token", tokenValue);
+            //IRestResponse response = client.Execute(request);
+            var asynHandler = client.ExecuteAsync(request, requestResponse =>
+            {
+
+                try
+                {
+                    Boolean booll = DevlessBuilder.checkAuth(requestResponse.Content);
+                    //Console.WriteLine(booll);
+                    if (booll)
+                    {
+                        Boolean successful = DevlessBuilder.checkGetSuccess(requestResponse.Content);
+                        if (successful)
+                        {
+                            //var Payload = DevLessBuilder.GetPayload(requestResponse.Content);
+                            DevlessBuilder.GetResults(DevlessBuilder.GetPayload(requestResponse.Content));
+
+                        }
+                        else
+                        {
+                            string errorMessage = "Error: The ServiceName or TableName doesn't exist";
+                            Console.WriteLine(errorMessage);
+                        }
+                    }
+
+                    else
+                    {
+                        //requestResponse.userNotAuthenticated(new ErrorMessage("Token expired please log in again"));
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+
+            });
+            //var output = response.Content;
+            // return response.Content;
+
+
+        }
+
+        public void postData(String serviceName, String tableName, IDictionary<String, Object> datatoAdd)
+        {
+            //using RESTSHARP
+            var client = new RestClient(new DevlessBuilder().postUrl(rootUrl, serviceName));
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("devless-token", tokenValue);
+            var datatosend = DevlessBuilder.createPostBody(tableName, datatoAdd);
+            //short hand
+            request.AddJsonBody(datatosend);
+            //longhand
+            //request.RequestFormat = DataFormat.Json;
+            //request.AddBody(datatoAdd);
+            //executing the request
+            IRestResponse response = client.Execute(request);
+            var output = response.Content;
+            Console.WriteLine(output);
+
+            // var asynHandler = client.ExecuteAsync(request, response => { Console.WriteLine(response); });
+
+        }
+
+        public void edit(String serviceName, String tableName, IDictionary<String, Object> update, String id)
+        {
+            var client = new RestClient(new DevlessBuilder().postUrl(rootUrl, serviceName));
+            var request = new RestRequest(Method.PATCH);
+            request.AddHeader("devless-token", tokenValue);
+            var datatosend = DevlessBuilder.createPatchBody(tableName, update, id);
+            request.AddJsonBody(datatosend);
+            IRestResponse response = client.Execute(request);
+            var output = response.Content;
+            Console.WriteLine(output);
+        }
+
+        public void delete(String serviceName, String tableName, String id)
+        {
+            var client = new RestClient(new DevlessBuilder().postUrl(rootUrl, serviceName));
+            var request = new RestRequest(Method.DELETE);
+            request.AddHeader("devless-token", tokenValue);
+            var datatosend = DevlessBuilder.createDeleteBody(tableName, id);
+            request.AddJsonBody(datatosend);
+            IRestResponse response = client.Execute(request);
+            var output = response.Content;
+            Console.WriteLine(output);
+
+        }
+
+        public void deleteAll(String serviceName, String tableName)
+        {
+            var client = new RestClient(new DevlessBuilder().postUrl(rootUrl, serviceName));
+            var request = new RestRequest(Method.DELETE);
+            request.AddHeader("devless-token", tokenValue);
+            var datatosend = DevlessBuilder.createDeleteAllBody(serviceName, tableName);
+            request.AddJsonBody(datatosend);
+            IRestResponse response = client.Execute(request);
+            var output = response.Content;
+            Console.WriteLine(output);
+
+
+
+        }
+    }
+}
---
+++ DvSharpSdk/DvSharpSdk/Util/DevlessBuilder.cs
@@ -0,0 +1,200 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DvSharpSdk.Util
+{
+   public class DevlessBuilder
+    {
+
+        public String formUrl(String rootUrl, String serviceName)
+        {
+            return rootUrl + "/api/v1/service/" + serviceName + "/";
+        }
+
+        public String getUrl(String rootUrl, String serviceName, String tableName)
+        {
+            return rootUrl + "/api/v1/service/" + serviceName + "/db?table=" + tableName;
+        }
+
+        public String postUrl(String rootUrl, String serviceName)
+        {
+            return rootUrl + "/api/v1/service/" + serviceName + "/db";
+        }
+
+        public static Boolean checkAuth(String response)
+        {
+
+            Boolean booll = false;
+            try
+            {
+                var result = JsonConvert.DeserializeObject(response);
+                JObject obj = JObject.Parse(result.ToString());
+                string statuscode = obj["status_code"].ToString(); int statusCode = Int32.Parse(statuscode);
+                if (statusCode == 625 || statusCode == 609 || statusCode == 619 || statusCode == 636 || statusCode == 614 || statusCode == 1000)
+                {
+                    booll = true;
+                }
+                else if (statusCode == 628)
+                {
+                    booll = false;
+                }
+                else
+                {
+                    booll = true;
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return booll;
+        }
+
+        public static Boolean checkGetSuccess(String response)
+        {
+            Boolean booll = false;
+            try
+            {
+                var result = JsonConvert.DeserializeObject(response);
+                JObject obj = JObject.Parse(result.ToString());
+                string statuscode = obj["status_code"].ToString(); int statusCode = Int32.Parse(statuscode);
+                if (statusCode == 634 || statusCode == 604)
+                {
+                    booll = false;
+                }
+                else
+                {
+                    booll = true;
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return booll;
+
+        }
+
+        public static string GetPayload(String response)
+        {
+            var result = JsonConvert.DeserializeObject(response);
+            JObject obj = JObject.Parse(result.ToString());
+            string payload = obj["payload"].ToString();
+            if (payload != "")
+            {
+                var msg = "Payload available";
+            }
+            else
+            {
+                var msg = "Payload not available";
+            }
+
+            return payload;
+
+        }
+
+        public static void GetResults(String payload)
+        {
+
+            JObject obj = JObject.Parse(payload);
+            JArray rect = (JArray)obj["results"];
+             
+
+        }
+
+        public static IDictionary<String, Object> createPostBody(String table_name, IDictionary<String, Object> fieldMap)
+        {
+            Dictionary<String, Object> map = new Dictionary<String, Object>();
+            ArrayList mainList = new ArrayList();
+            Dictionary<String, Object> miniMap = new Dictionary<String, Object>();
+            ArrayList fieldList = new ArrayList();
+            fieldList.Add(fieldMap);
+            miniMap.Add("name", table_name);
+            miniMap.Add("field", fieldList);
+            mainList.Add(miniMap);
+            map.Add("resource", mainList);
+
+            return map;
+        }
+
+        public static IDictionary<String, Object> createPatchBody(String table_name, IDictionary<String, Object> dataChange, String id)
+        {
+            Dictionary<String, Object> map = new Dictionary<String, Object>();
+            ArrayList mainList = new ArrayList();
+            Dictionary<String, Object> miniMap = new Dictionary<String, Object>();
+            ArrayList fieldList = new ArrayList();
+            Dictionary<String, Object> fieldMap = new Dictionary<String, Object>();
+            fieldMap.Add("where", "id," + id);
+            ArrayList dataFields = new ArrayList();
+            dataFields.Add(dataChange);
+            fieldMap.Add("data", dataFields);
+            fieldList.Add(fieldMap);
+
+            miniMap.Add("name", table_name);
+            miniMap.Add("params", fieldList);
+            mainList.Add(miniMap);
+            map.Add("resource", mainList);
+            return map;
+        }
+
+        public static IDictionary<String, Object> createDeleteBody(String table_name, String id)
+        {
+            Dictionary<String, Object> map = new Dictionary<String, Object>();
+            ArrayList mainList = new ArrayList();
+            Dictionary<String, Object> miniMap = new Dictionary<String, Object>();
+            ArrayList fieldList = new ArrayList();
+            Dictionary<String, Object> fieldMap = new Dictionary<String, Object>();
+
+            fieldMap.Add("delete", "true");
+            fieldMap.Add("where", "id,=," + id);
+            fieldList.Add(fieldMap);
+            miniMap.Add("name", table_name);
+            miniMap.Add("params", fieldList);
+            mainList.Add(miniMap);
+            map.Add("resource", mainList);
+            return map;
+
+        }
+
+        public static IDictionary<String, Object> createDeleteAllBody(String serviceName, String tableName)
+        {
+            Dictionary<String, Object> map = new Dictionary<String, Object>();
+            ArrayList mainList = new ArrayList();
+            Dictionary<String, Object> miniMap = new Dictionary<String, Object>();
+            ArrayList fieldList = new ArrayList();
+            Dictionary<String, Object> fieldMap = new Dictionary<String, Object>();
+
+            fieldMap.Add("delete", "true");
+            fieldList.Add(fieldMap);
+            miniMap.Add("name", tableName);
+            miniMap.Add("params", fieldList);
+            mainList.Add(miniMap);
+            map.Add("resource", mainList);
+            return map;
+        }
+
+        public static IDictionary<String, Object> signUpBuilder(String userName, String email, String password, String phoneNumber, String firstName, String lastName)
+        {
+            Dictionary<String, Object> signUp = new Dictionary<String, Object>();
+            ArrayList paramss = new ArrayList() { "email", "password", "userName", "phoneNumber", "firstName", "lastName", "null" };
+
+            signUp.Add("params", paramss);
+            signUp.Add("jsonrpc", "2.0");
+            signUp.Add("method", "devless");
+            signUp.Add("id", "1000");
+
+            return signUp;
+        }
+
+
+
+    }
+}
